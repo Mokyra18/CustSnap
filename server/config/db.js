683b8641375941a66255e54433f7654d54c172a2
M Rizky Ramadhani <69834119+Mokyra18@users.noreply.github.com>
2023-12-28 21:54:34 +0700

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
 const connectDB = async()=> {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`Database Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
